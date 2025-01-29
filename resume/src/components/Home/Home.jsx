417a7fca9a23e677b836e1861062e2563e380f9b
Bishuthapa <thapabishu435@gmail.com>
2025-01-30 00:02:39 +0545

    initial commit

--- resume/src/components/Home/Home.jsx
+++ resume/src/components/Home/Home.jsx
@@ -1,7 +1,7 @@
 function Home() {
   return (
     <div className="h-auto w-full bg-zinc-900 text-white">
-      <div className="my-20 mx-22 font-bold text-xl">
+      <div className="my-20 mx-22 font-bold text-3xl flex justify-center">
         <h1>Hi, I&apos;m Bishesh Thapa </h1>
               </div>
     </div>
--- resume/src/components/Navigation/Navigation.jsx
+++ resume/src/components/Navigation/Navigation.jsx
@@ -1,37 +1,35 @@
 import { NavLink } from "react-router-dom";
 
 function Navigation() {
-return (
-    <nav>
-        <div className="flex justify-between p-6 px-40 w-full bg-zinc-900 text-white font-bold text-2xl">
-            <ul >
-                <li>
-                    <NavLink to="/"
-                    //className={({ isActive }) =>
-                        //isActive ? "underline decoration-2 underline-offset-3 transition-all duration-200" : ""}
-                    >Bishesh</NavLink>
-                </li>
-            </ul>
-            <ul className="flex gap-10">
-                <li>
-                    <NavLink to="/about"
-                    className={({ isActive }) =>
-                        isActive ? "underline decoration-2 underline-offset-3 transition-all duration-200" : ""
-                    }
-                    >About</NavLink>
-                </li>
-                <li>
-                    <NavLink to="/blog"
-                    className={({ isActive }) =>
-                        isActive ? "underline decoration-2 underline-offset-3 transition-all duration-200" : ""
-                    }
-                    >Blog</NavLink>
-                </li>
-                
-            </ul>
-        </div>
-    </nav>
-);
+    return (
+        <nav>
+            <div className="flex justify-between p-6 lg:px-70 w-full bg-zinc-900 text-white font-bold text-2xl">
+                <ul className="mr-15">
+                    <li>
+                        <NavLink to="/"
+                         
+                        >Bishesh</NavLink>
+                    </li>
+                </ul>
+                <ul className="flex gap-10">
+                    <li>
+                        <NavLink to="/about"
+                        className={({ isActive }) =>
+                            isActive ? "underline decoration-2 underline-offset-3 transition-all duration-200" : ""
+                        }
+                        >About</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/blog"
+                        className={({ isActive }) =>
+                            isActive ? "underline decoration-2 underline-offset-3 transition-all duration-200" : ""
+                        }
+                        >Blog</NavLink>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    );
 }
 
 export default Navigation;
